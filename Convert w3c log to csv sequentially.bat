47a1bfa81d941d156b65ae766d431290fe6c18fa
Kenneth Teo <kennetht@microsoft.com>
2018-04-27 17:37:29 +0800

    no comment

--- Convert w3c log to csv sequentially.bat
+++ Convert w3c log to csv sequentially.bat
@@ -1,4 +1,4 @@
- cd "[logs folder location]"
+ cd "C:\mstemp\Blog\Sample W3C IIS Logs"
  for /R %%f in (*.log) do (
  "C:\Program Files (x86)\Log Parser 2.2\"logparser -i:W3C -o:csv "SELECT * INTO '%%f.csv' FROM '%%f'"
  )
